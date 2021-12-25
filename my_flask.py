20a6ed30d78f024633a3fa7917e0c80548bf7411
koshi8bit 010 <koshi8bit@mail.ru>
2021-12-25 13:55:15 +0700

    integration fine

--- my_flask.py
+++ my_flask.py
@@ -1,3 +1,4 @@
+from threading import Thread, Lock
 from flask import Flask, jsonify, abort, request
 import logging
 import threading
@@ -13,6 +14,7 @@ import json
 app = Flask(__name__)
 
 results = []
+mutex = Lock()
 
 @app.route('/HMC/qr', methods=['POST'])
 def get_task():
@@ -22,12 +24,12 @@ def get_task():
     if not validate_qr(request.json['qr']):
         logging.error(f'422')
         return jsonify({'status': 'Unprocessable Entity'}), 422
-    logging.info(f'incoming.. id={request.json["id"]}; qr={request.json["qr"]}')
-
-
     global results
-    results.append(ret)    
-    logging.info(f'work.add_data ok..')
+    global mutex 
+    mutex.acquire()
+    results.append([request.json["id"], request.json["qr"]])    
+    mutex.release()
+    logging.info(f'incoming.. id={request.json["id"]}; qr={request.json["qr"]}')
     return jsonify({'status': 'Ok'}), 200
 
 def request_to_fns(qr_code):
@@ -40,7 +42,7 @@ def back_url():
     env_var = os.getenv("URL_TO_BACK")
     if not env_var:
         logging.error("Empty url")
-        raise ValueError('Variable "URL_TO_BACK" is not set')
+#        raise ValueError('Variable "URL_TO_BACK" is not set')
 
     res =  f'{env_var}hmc/api/v1/fns/qr-code-response'
     logging.info(f'url = {res}')
@@ -59,31 +61,33 @@ def start_timer():
     threading.Timer(5, timer_callback).start()
 
 def timer_callback():
-    logging.info(f'timer_callback')
-    global back_url
-    global results
-    for res in results:
-        try:
-            client = NalogRuPython()
-            ticket = client.get_ticket(request.json["qr"])
-            print(ticket)
+    global mutex
+    with mutex:
+        global results
+        logging.info(f'timer_callback {len(results)}')
+        global back_url
+        for pair in results:
+            try:
+                client = NalogRuPython()
+                ticket = client.get_ticket(pair[1])
+                logging.info('ticket ready')
+                ret = dict({'id': pair[0], "receipt": ticket, 'status': 'OK'})
 
-            ret = dict({'id': request.json["id"], "receipt": ticket, 'status': 'OK'})
-            logging.info('try ok')
-        except Exception as ex:
-            ret = dict({'id': request.json["id"], "receipt": '', 'status': 'ERROR'})
-            logging.info('try exc')
+            except Exception as ex:
+                ret = dict({'id': pair[0], "receipt": '', 'status': 'ERROR'})
+                logging.error('try exc')
 
-        resp = requests.post(back_url, json=res)
-        # logging.info(str(json.dumps(ticket, indent=4, ensure_ascii=False)))
-        logging.info(f'--------------------------------------------')
-        logging.info(str(json.dumps(res, indent=4, ensure_ascii=False)))
-        logging.info(f'resp: {str(resp)}')
-        logging.info(f'resp.text: {str(resp.text)}')
-        logging.info(f'resp.status_code: {str(resp.status_code)}')
-    #    logging.info(str(json.dumps(resp.json(), indent=4, ensure_ascii=False)))
+            resp = requests.post(back_url, json=ret)
+            # logging.info(str(json.dumps(ticket, indent=4, ensure_ascii=False)))
+            logging.info(f'--------------------------------------------')
+            logging.info(str(json.dumps(ret, indent=4, ensure_ascii=False)))
+            logging.info(f'resp: {str(resp)}')
+            logging.info(f'resp.text: {str(resp.text)}')
+            logging.info(f'resp.status_code: {str(resp.status_code)}')
+        #    logging.info(str(json.dumps(resp.json(), indent=4, ensure_ascii=False)))
 
-    start_timer()
+        results.clear()
+        start_timer()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.NOTSET, format='%(asctime)s - %(levelname)s - %(message)s',
